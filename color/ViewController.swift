7cb497f1ed0835287fd5666dda64d02167442d56
Elizabeth Stoliker <elizabethstoliker@elizabeths-imac.local>
2016-07-23 20:42:34 -0600

    Initial Commit

---
+++ color/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  color
+//
+//  Created by Elizabeth Stoliker on 7/23/16.
+//  Copyright © 2016 Amdahl_Coding. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
