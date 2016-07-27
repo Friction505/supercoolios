1ee51d62d933eb89d1088b905f7b1d9782058ac7
Elizabeth Stoliker <elizabethstoliker@Elizabeths-iMac.local>
2016-07-26 20:02:10 -0600

    show me what you got

--- color/ViewController.swift
+++ color/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Angry_dog: UIButton!
+    @IBOutlet weak var happy_dog: UIButton!
+    @IBOutlet weak var happy_photo: UIImageView!
+    @IBOutlet weak var Angery_photo: UIImageView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +24,22 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func hideAngery(sender: UIButton) {
+        if Angery_photo.hidden == false{
+            Angery_photo.hidden = true}
+        else{
+            Angery_photo.hidden = false}
+        }
+    @IBAction func hideHappy(sender: UIButton) {
+        if happy_photo.hidden == false{
+            happy_photo.hidden = true}
+        else{
+            happy_photo.hidden = false}
+    }
 }
+    
+
+
+
 
